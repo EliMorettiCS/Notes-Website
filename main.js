bde7b71c7597bef69efea577ceee5ceb536f8073
Eli Moretti <144716146+EliMorettiCS@users.noreply.github.com>
2025-12-08 13:41:22 -0800

    Made settings functional.

--- main.js
+++ main.js
@@ -122,3 +122,140 @@ cloudNotes.forEach((cloudNote) => {
 })
 
 selectNote(notes.length-1)
+
+let highContrast = false;
+document.getElementById("highContrast").classList.remove("on")
+document.getElementById("highContrast").classList.add("off")
+document.getElementById("highContrast").onclick = function() {
+    if (highContrast){
+        highContrast = false;
+    }
+    else {
+        highContrast = true;
+    }
+
+    if (highContrast) {
+        document.getElementById("highContrast").classList.remove("off")
+        document.getElementById("highContrast").classList.add("on")
+        document.documentElement.style.setProperty("--mainDark","rgba(0, 0, 0, 1)");
+        document.documentElement.style.setProperty("--main","rgba(255, 255, 255, 1)");
+        document.documentElement.style.setProperty("--mainLight","rgba(0, 0, 0, 1)");
+        document.documentElement.style.setProperty("--blackGrey","rgb(15, 15, 15)");
+        document.documentElement.style.setProperty("--whiteGrey","rgba(0, 0, 0, 1)");
+    }
+    else {
+        document.getElementById("highContrast").classList.remove("on")
+        document.getElementById("highContrast").classList.add("off")
+        document.documentElement.style.setProperty("--mainDark","rgb(27,27,27)");
+        document.documentElement.style.setProperty("--main","rgb(47, 47, 47)");
+        document.documentElement.style.setProperty("--mainLight","rgb(87,87,87)");
+        document.documentElement.style.setProperty("--blackGrey","rgb(15, 15, 15)");
+        document.documentElement.style.setProperty("--whiteGrey","rgb(240, 240, 240)");
+    }
+}
+
+let colorIndependance = false;
+document.getElementById("colorIndependance").classList.remove("on")
+document.getElementById("colorIndependance").classList.add("off")
+document.getElementById("colorIndependance").onclick = function() {
+    if (colorIndependance){
+        colorIndependance = false;
+    }
+    else {
+        colorIndependance = true;
+    }
+    if (colorIndependance){
+        document.getElementById("colorIndependance").classList.remove("off")
+        document.getElementById("colorIndependance").classList.add("on")
+        document.documentElement.style.setProperty("--colorDependentBorder","dashed");
+    }
+    else {
+        document.getElementById("colorIndependance").classList.remove("on")
+        document.getElementById("colorIndependance").classList.add("off")
+        document.documentElement.style.setProperty("--colorDependentBorder","solid");
+    }
+}
+
+let animatedElements = true;
+document.getElementById("animatedElements").classList.remove("off")
+document.getElementById("animatedElements").classList.add("on")
+document.getElementById("animatedElements").onclick = function() {
+    if (animatedElements){
+        animatedElements = false;
+    }
+    else {
+        animatedElements = true;
+    }
+    if (animatedElements){
+        document.getElementById("animatedElements").classList.remove("off")
+        document.getElementById("animatedElements").classList.add("on")
+        document.documentElement.style.setProperty("--animations","all");
+    }
+    else {
+        document.getElementById("animatedElements").classList.remove("on")
+        document.getElementById("animatedElements").classList.add("off")
+        document.documentElement.style.setProperty("--animations","none");
+    }
+}
+
+let color = 1;
+document.getElementById("color").onclick = function() {
+    if (color == 5){
+        color = 1;
+    }
+    else{
+        color += 1;
+    }
+    if (color==1){
+        document.documentElement.style.setProperty("--accent","rgba(255, 127, 127, 1)");
+    }
+    if (color==2){
+        document.documentElement.style.setProperty("--accent","rgba(129, 127, 255, 1)");
+    }
+    if (color==3){
+        document.documentElement.style.setProperty("--accent","rgba(185, 255, 185, 1)");
+    }
+    if (color==4){
+        document.documentElement.style.setProperty("--accent","rgba(250, 163, 255, 1)");
+    }
+    if (color==5){
+        document.documentElement.style.setProperty("--accent","rgba(255, 176, 127, 1)");
+    }
+}
+
+let roundness = 15;
+document.getElementById("roundness").onclick = function() {
+    if (roundness == 15){
+        roundness = 0;
+    }
+    else if (roundness == 0){
+        roundness = 5
+    }
+    else if (roundness == 5){
+        roundness = 10
+    }
+    else if (roundness == 10){
+        roundness = 15
+    }
+    document.documentElement.style.setProperty("--globalRadius",roundness+"px");
+}
+
+let soundEffects = false;
+document.getElementById("soundEffects").onclick = function() {
+    if (soundEffects){
+        soundEffects = false;
+    }
+    else {
+        soundEffects = true;
+    }
+}
+
+let backgroundMusic = false;
+document.getElementById("backgroundMusic").onclick = function() {
+    if (backgroundMusic){
+        backgroundMusic = false;
+    }
+    else {
+        backgroundMusic = true;
+    }
+}
